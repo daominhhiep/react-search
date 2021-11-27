6f06e22283b7b400da75187a8367e2361b482674
Dao Minh Hiep <daominhhiep97@gmail.com>
2021-11-27 10:14:16 +0700

    init

---
+++ src/data/initialDetails.js
@@ -0,0 +1,25 @@
+const initialDetails = [
+    {
+        id: 1,
+        imgPath: "/assets/img/1.jpg",
+        name: "Minh Nguyen",
+        email: "kattobi97@gmail.com",
+        address: "Ha Noi, Viet Nam",
+    },
+    {
+        id: 2,
+        imgPath: "/assets/img/2.jpg",
+        name: "Tuan Nguyen",
+        email: "tubo@yahoo.com.vn",
+        address: "Bac Ninh, Viet Nam",
+    },
+    {
+        id: 3,
+        imgPath: "/assets/img/3.jpg",
+        name: "Hung Tran",
+        email: "hungtran@gmail.vn",
+        address: "Ha Noi, Viet Nam",
+    }
+];
+
+export default initialDetails;
